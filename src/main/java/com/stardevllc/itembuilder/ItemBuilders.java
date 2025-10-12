7038183aa5e73b7236ffba453ee69476d63ffe3b
Firestar311 <slaterandrew0398@gmail.com>
2025-10-12 00:57:48 -0500

    Moved the ItemBuilders class and made a loading init method that will catch the errors if a meta class is not found

--- src/main/java/com/stardevllc/itembuilder/ItemBuilders.java
+++ src/main/java/com/stardevllc/itembuilder/ItemBuilders.java
@@ -1,10 +1,21 @@
 package com.stardevllc.itembuilder;
 
+import com.stardevllc.itembuilder.v1_13.FishBucketBuilder;
+import com.stardevllc.itembuilder.v1_13_2.MapItemBuilder;
+import com.stardevllc.itembuilder.v1_14_4.*;
+import com.stardevllc.itembuilder.v1_16_1.BookItemBuilder;
+import com.stardevllc.itembuilder.v1_16_1.CompassItemBuilder;
+import com.stardevllc.itembuilder.v1_17_1.AxolotlItemBuilder;
+import com.stardevllc.itembuilder.v1_19_3.GoatHornBuilder;
+import com.stardevllc.itembuilder.v1_20_1.ArmorItemBuilder;
+import com.stardevllc.itembuilder.v1_21_1.OminousBottleBuilder;
+import com.stardevllc.itembuilder.v1_21_1.WritableBookBuilder;
+import com.stardevllc.itembuilder.v1_8.*;
 import com.stardevllc.smaterial.SMaterial;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.*;
 
 import java.lang.reflect.Constructor;
 import java.util.HashMap;
@@ -12,6 +23,53 @@ import java.util.Map;
 
 public final class ItemBuilders {
     private static final Map<Class<? extends ItemMeta>, Class<? extends ItemBuilder<?, ?>>> META_TO_BUILDERS = new HashMap<>();
+    
+    public static void init() {
+        //Due to how java class files are, the imports are not a major problem and this should be fine
+        try {
+            ItemBuilders.mapMetaToBuilder(BannerMeta.class, BannerItemBuilder.class);
+            ItemBuilders.mapMetaToBuilder(EnchantmentStorageMeta.class, EnchantedBookBuilder.class);
+            ItemBuilders.mapMetaToBuilder(FireworkMeta.class, FireworkItemBuilder.class);
+            ItemBuilders.mapMetaToBuilder(FireworkEffectMeta.class, FireworkStarBuilder.class);
+            ItemBuilders.mapMetaToBuilder(SkullMeta.class, SkullItemBuilder.class);
+        } catch (Throwable e) {}
+        
+        try {
+            ItemBuilders.mapMetaToBuilder(TropicalFishBucketMeta.class, FishBucketBuilder.class);
+        } catch (Throwable e) {}
+        
+        try {
+            ItemBuilders.mapMetaToBuilder(MapMeta.class, MapItemBuilder.class);
+        } catch (Throwable e) {}
+        
+        try {
+            ItemBuilders.mapMetaToBuilder(CrossbowMeta.class, CrossbowItemBuilder.class);
+            ItemBuilders.mapMetaToBuilder(SuspiciousStewMeta.class, StewItemBuilder.class);
+            ItemBuilders.mapMetaToBuilder(BlockDataMeta.class, BlockDataItemBuilder.class);
+        } catch (Throwable e) {}
+        
+        try {
+            ItemBuilders.mapMetaToBuilder(BookMeta.class, BookItemBuilder.class);
+            ItemBuilders.mapMetaToBuilder(CompassMeta.class, CompassItemBuilder.class);
+        } catch (Throwable e) {}
+        
+        try {
+            ItemBuilders.mapMetaToBuilder(AxolotlBucketMeta.class, AxolotlItemBuilder.class);
+        } catch (Throwable e) {}
+        
+        try {
+            ItemBuilders.mapMetaToBuilder(MusicInstrumentMeta.class, GoatHornBuilder.class);
+        } catch (Throwable e) {}
+        
+        try {
+            ItemBuilders.mapMetaToBuilder(ArmorMeta.class, ArmorItemBuilder.class);
+        } catch (Throwable e) {}
+        
+        try {
+            ItemBuilders.mapMetaToBuilder(OminousBottleMeta.class, OminousBottleBuilder.class);
+            ItemBuilders.mapMetaToBuilder(WritableBookMeta.class, WritableBookBuilder.class);
+        } catch (Throwable e) {}
+    }
     
     public static <M extends ItemMeta> void mapMetaToBuilder(Class<M> meta, Class<? extends ItemBuilder<?, M>> builder) {
         META_TO_BUILDERS.put(meta, builder);
